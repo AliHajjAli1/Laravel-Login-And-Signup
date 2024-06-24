b7ef9fac6e1cf846f7d6890f0b020ce729b640aa
Ali Al Hajj Ali <alihjali2004@gmail.com>
2024-06-24 11:39:35 +0300

    New Additions

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile Page</title>
+    <style>
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+            background-color: white;
+        }
+        .container {
+            text-align: center;
+        }
+        .loginlabel {
+            font-weight: 700;
+            color: rgb(220, 38, 38);
+            font-size: 1.5rem;
+            margin-bottom: 1rem;
+        }
+        .logininput {
+            border-width: 1px;
+            --tw-border-opacity: 1;
+            border-color: rgb(252 165 165 / var(--tw-border-opacity));
+            border-radius: 1rem/* 16px */;
+            margin-top: 0.5rem/* 8px */;
+            margin-bottom: 0.5rem/* 8px */;
+            width: 100%;
+            height: 2rem/* 16px */;
+        }
+        .passwordinput {
+            border-width: 1px;
+            --tw-border-opacity: 1;
+            border-color: rgb(252 165 165 / var(--tw-border-opacity));
+            border-radius: 1rem/* 16px */;
+            margin-top: 0.5rem/* 8px */;
+            margin-bottom: 0.5rem/* 8px */;
+            width: 100%;
+            height: 2rem/* 16px */;
+        }
+        .loginbutton {
+            border-radius: 1rem;
+            background-color: rgb(220, 38, 38);
+            color: white;
+            border: none;
+            margin-top: 0.5rem;
+            width: 100%;
+            height: 2rem;
+            display: block;
+            cursor: pointer;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="logindiv">
+            <label class="loginlabel">My Profile</label>
+            @if ($errors->any())
+                <div>
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+                <input type="text" name="name" placeholder="Name" class="logininput" value="{{ old('name') }}"/>
+                <input type="text" name="phone" placeholder="Phone" class="logininput" value="{{ old('phone') }}"/>
+                <input type="text" name="address" placeholder="Address" class="logininput" value="{{ old('address') }}"/>
+                <button type="submit" class="loginbutton">Done</button>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,15 @@
 <?php
 use App\Http\Controllers\SignupController;
 use App\Http\Controllers\LoginController;
+use App\Http\Middleware\CheckName;
 
 Route::get('/home', function () {
-    return view('welcome');
+    return view('profile');
 })->name('home');
+Route::get('/signup', function () {
+    return view('signup');
+})->middleware(CheckName::class);
+
 
 Route::get('/signup', [SignupController::class, 'showSignupForm'])->name('signup.form');
 Route::post('/signup', [SignupController::class, 'signup'])->name('signup');
